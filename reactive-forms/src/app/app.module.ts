bbf4027732d44996fb8173265289f460e159475a
Yatish Kelkar <yatish1606@gmail.com>
2021-05-20 13:36:50 +0530

    Add forms

--- reactive-forms/src/app/app.module.ts
+++ reactive-forms/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ReactFormComponent } from './react-form/react-form.component';
 import { DynamicFormQuestionComponent } from './dynamic-form-question/dynamic-form-question.component';
+import { DynamicFormComponent } from './dynamic-form/dynamic-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ReactFormComponent,
-    DynamicFormQuestionComponent
+    DynamicFormQuestionComponent,
+    DynamicFormComponent
   ],
   imports: [
     BrowserModule,
--- reactive-forms/src/app/dynamic-form-question/dynamic-form-question.component.ts
+++ reactive-forms/src/app/dynamic-form-question/dynamic-form-question.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { QuestionBase } from '../question-base';
+
 
 @Component({
-  selector: 'app-dynamic-form-question',
-  templateUrl: './dynamic-form-question.component.html',
-  styleUrls: ['./dynamic-form-question.component.css']
+  selector: 'app-question',
+  templateUrl: './dynamic-form-question.component.html'
 })
-export class DynamicFormQuestionComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class DynamicFormQuestionComponent {
+  @Input() question!: QuestionBase<string>;
+  @Input() form!: FormGroup;
+  get isValid() { return this.form.controls[this.question.key].valid; }
 }
